d12a5c3fc99cbe029091bd8007c3c2bf6affced1
Amber Anand <amber.1rn14is015@gmail.com>
2020-03-27 15:54:39 +0530

    Updates

---
+++ precache-manifest.f3d8379bd6d0e5e9787de3dc2269cd20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce73283430ab1f31652cac7499eb3132",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "941ec94ca64116137681",
+    "url": "/covid-19/static/css/main.9b16cba0.chunk.css"
+  },
+  {
+    "revision": "22b2a282129fb6771592",
+    "url": "/covid-19/static/js/2.375da88a.chunk.js"
+  },
+  {
+    "revision": "5eefe3d988907fb680c439c15ee4c323",
+    "url": "/covid-19/static/js/2.375da88a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941ec94ca64116137681",
+    "url": "/covid-19/static/js/main.9dcd91d3.chunk.js"
+  },
+  {
+    "revision": "9d0569c156b0c18b94d0",
+    "url": "/covid-19/static/js/runtime-main.5130bf54.js"
+  }
+]);
